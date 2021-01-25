74c971faaaf83316e8d290220d2a9695a2eac5ac
yuxin <799746606@qq.com>
2021-01-25 23:51:14 +0800

    #增加头像以及退出登录功能，优化登录逻辑

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -7,9 +7,25 @@ const login = ({ commit }, userInfo = {}) => {
       commit('USERINFO_SET', userInfo)
       cookie.set(constant.COOKIE_USERINFO, userInfo, { expires: constant.COOKIE_USERINFO_EXPIRE })
       resolve(userInfo)
+    } else {
+      reject(new Error('账号密码错误'))
     }
+  }).catch(() => {
+    throw new Error('登录发生异常，请联系管理员')
   })
 }
+
+const logout = ({ commit }) => {
+  return new Promise((resolve) => {
+    commit('USERINFO_SET')
+    cookie.remove(constant.COOKIE_USERINFO)
+    resolve('已退出登录')
+  }).catch(() => {
+    throw new Error('退出登录发生异常，请联系管理员')
+  })
+}
+
 export default {
-  login
+  login,
+  logout
 }
