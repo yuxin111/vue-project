77ef700a08ae2e872440a33c4192f4e2a497945e
yuxin <799746606@qq.com>
2021-03-02 23:38:12 +0800

    #初始化角色管理页面

---
+++ src/views/system/role/Index.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="system-role">
+
+    <!--  查询参数  -->
+    <div class="search text-center">
+      <el-form :model="search" label-width="80px" inline>
+        <el-form-item label="登录账号">
+          <el-input v-model="search.loginName" placeholder="请输入登录账号" size="small"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="el-icon-search" size="mini" @click="queryUserList">搜索</el-button>
+          <el-button icon="el-icon-refresh" size="mini" @click="resetSearch">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <!--  工具栏  -->
+    <div class="tools flex-space-between flex-align-center">
+      <div class="tools-btn">
+        <el-button icon="el-icon-plus" type="primary" size="small" plain @click="addUserInfo">新增</el-button>
+      </div>
+      <div class="tools-opera">
+        <el-tooltip class="item" effect="dark" content="刷新" placement="top">
+          <el-button type="primary" size="small" circle><i class="el-icon-refresh"/></el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="显隐列" placement="top-end">
+          <el-button type="primary" size="small" circle><i class="el-icon-menu"/></el-button>
+        </el-tooltip>
+      </div>
+    </div>
+
+    <!--  角色表格  -->
+    <div class="table m-t-15">
+      <el-table
+        v-loading="tableLoading"
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="loginName"
+          label="登录账号"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="createTime"
+          label="创建时间"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="updateTime"
+          label="最后更新时间"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          align="center">
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="watchUserInfo(scope.row)">查看</el-button>
+            <el-button type="text" size="small" @click="editUserInfo(scope.row)">编辑</el-button>
+            <el-popconfirm
+              title="是否删除该角色信息？"
+              icon="el-icon-info"
+              icon-color="red"
+              placement="top"
+              @confirm="deleteUserInfo(scope.row)"
+            >
+              <el-button type="text" size="small" slot="reference" class="m-l-10">删除</el-button>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <!--  分页  -->
+    <div class="pagination flex-justify-end m-t-15">
+      <el-pagination
+        v-show="pagination.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagination.pageNum"
+        :page-sizes="[5, 10, 20, 50]"
+        :page-size="pagination.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pagination.total">
+      </el-pagination>
+    </div>
+
+    <!--  角色信息dialog  -->
+    <el-dialog
+      :title="
+        operaStatus === 'add' ? '新增角色' :
+        operaStatus === 'edit' ? '修改角色' : '角色信息'"
+      :visible.sync="userDialog.visible"
+      width="600px">
+      <SystemUserInfo
+        :propData="userDialog.data"
+        :confirmLoading="userDialog.confirmLoading"
+        @confirm="confirmUserInfo"
+        @cancel="userDialog.visible = false"
+      />
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import SystemUserInfo from './component/SystemUserInfo'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: { SystemUserInfo },
+  data () {
+    return {
+      operaStatus: '', // 当前操作状态（'add'、'edit'、'watch'）
+      search: {
+        loginName: ''
+      },
+      pagination: {
+        pageSize: 10,
+        pageNum: 1,
+        total: null
+      },
+      userDialog: {
+        visible: false,
+        confirmLoading: false,
+        data: {}
+      },
+      tableData: [],
+      tableLoading: false
+    }
+  },
+  mounted () {
+    this.getUserList()
+  },
+  methods: {
+    getUserList () {
+      const params = {
+        loginName: this.search.loginName
+      }
+      this.tableLoading = true
+      this.$api.system.getUserList(this.pagination, params)
+        .then(res => {
+          this.pagination.total = res.total
+          this.tableData = res.data
+        })
+        .finally(() => {
+          this.tableLoading = false
+        })
+    },
+    queryUserList () {
+      this.getUserList()
+    },
+    addUserInfo () {
+      this.operaStatus = 'add'
+      this.userDialog.visible = true
+      this.userDialog.data = { _status: this.operaStatus }
+    },
+    watchUserInfo (row) {
+      this.operaStatus = 'watch'
+      this.userDialog.visible = true
+      this.userDialog.data = this._.merge(
+        { _status: this.operaStatus },
+        this._.cloneDeep(row)
+      )
+    },
+    editUserInfo (row) {
+      this.operaStatus = 'edit'
+      this.userDialog.visible = true
+      this.userDialog.data = this._.merge(
+        { _status: this.operaStatus },
+        this._.cloneDeep(row)
+      )
+    },
+    deleteUserInfo (row) {
+      this.$api.system.deleteUser(row.userId)
+        .then(() => {
+          this.$message({
+            message: '删除角色信息成功',
+            type: 'success'
+          })
+          this.handleCurrentChange(1)
+        })
+    },
+    confirmUserInfo (formData) {
+      const params = {
+        ...formData,
+        createBy: formData._status === 'add' ? this.userInfo.username : formData.createBy,
+        updateBy: formData._status === 'edit' ? this.userInfo.username : formData.updateBy
+      }
+      const apiName = formData._status === 'add' ? 'addUser' : 'updateUser'
+      this.userDialog.confirmLoading = true
+      this.$api.system[apiName](params)
+        .then(res => {
+          this.$message({
+            message: '保存角色信息成功',
+            type: 'success'
+          })
+          this.userDialog.visible = false
+          this.handleCurrentChange(1)
+        })
+        .finally(() => {
+          this.userDialog.confirmLoading = false
+        })
+    },
+    refresh () {
+      this.resetSearch()
+      this.queryUserList()
+    },
+    resetSearch () {
+      this.search.loginName = ''
+    },
+    handleSizeChange (pageSize) {
+      this.pagination.pageSize = pageSize
+      this.pagination.pageNum = 1
+      this.queryUserList()
+    },
+    handleCurrentChange (pageNum) {
+      this.pagination.pageNum = pageNum
+      this.queryUserList()
+    }
+  },
+  computed: {
+    ...mapGetters({
+      userInfo: 'User/userInfo'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/system/role/component/SystemUserInfo.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="system-user-info">
+    <el-form :model="formData" :rules="rules" :ref="formName" label-width="80px">
+      <el-row>
+        <el-col :span="12" class="p-l-10">
+          <el-form-item label="登录账号" prop="loginName">
+            <el-input v-model="formData.loginName" placeholder="请输入登录账号"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12" class="p-l-10">
+          <el-form-item label="登录密码" :prop="formData._status === 'add' ? 'password' : null">
+            <el-input
+              type="password"
+              v-model="formData.password"
+              autocomplete="off"
+              :placeholder="formData._status === 'add' ? '请输入登录密码' : '不输入则不变'"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+
+    <span class="flex-justify-end">
+      <el-button type="primary" v-show="formData._status === 'edit' || formData._status === 'add'"
+                 @click="confirm()" :loading="loading"
+      >确 定</el-button>
+      <el-button @click="$emit('cancel')">取 消</el-button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    propData: {
+      type: Object,
+      default: () => {
+        return {
+          _status: 'watch',
+          loginName: '',
+          password: ''
+        }
+      }
+    },
+    confirmLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      formData: {},
+      formName: 'ruleForm',
+      loading: false,
+      rules: {
+        loginName: [
+          {
+            required: true,
+            message: '请输入登录账号名',
+            trigger: 'blur'
+          },
+          {
+            max: 30,
+            message: '长度不能大于30个字符',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入登录密码',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    confirm () {
+      this.$refs[this.formName].validate((valid) => {
+        if (valid) {
+          this.$emit('confirm', this.formData)
+        }
+      })
+    },
+    clearValidate () {
+      this.$nextTick(() => {
+        this.$refs[this.formName].clearValidate()
+      })
+    }
+  },
+  watch: {
+    propData: {
+      handler (propData) {
+        this.clearValidate()
+        this.formData = propData
+      },
+      immediate: true,
+      deep: true
+    },
+    confirmLoading: {
+      handler (confirmLoading) {
+        this.loading = confirmLoading
+      },
+      immediate: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
