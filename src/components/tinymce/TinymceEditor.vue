a7b5bd10e515c81c037946380c9d7fe14075d922
yuxin <799746606@qq.com>
2021-10-01 21:09:32 +0800

    #小修改

--- src/components/tinymce/TinymceEditor.vue
+++ src/components/tinymce/TinymceEditor.vue
@@ -46,7 +46,7 @@ export default {
         // 配置
         plugins: 'fullscreen link code table lists wordcount',
         toolbar: [
-          'formatselect fontselect fontsizeselect | bold italic forecolor backcolor lists table removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | fullscreen',
+          'formatselect fontselect fontsizeselect | bold italic forecolor backcolor lists table removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | fullscreen'
         ],
         fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
         font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
