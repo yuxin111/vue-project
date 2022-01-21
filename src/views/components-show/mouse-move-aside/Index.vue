db51277be3ca8c133f02c6c6bfe275390d1c3e7a
yuxin <799746606@qq.com>
2022-01-21 17:34:10 +0800

    #小修改

---
+++ src/views/components-show/mouse-move-aside/Index.vue
@@ -0,0 +1,54 @@
+/**
+* 鼠标移动触发侧边栏
+* author: yuxin
+* createtime: 2022-01-20
+* All rights reserved.
+*/
+<template>
+  <div class="mouse-move-aside">
+    <!--  触发区域  -->
+    <div class="emit-place" @mouseenter="show = true" @mouseleave="show = false"></div>
+    <!--  弹窗  -->
+    <transition>
+      <div class="windows-container"></div>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.mouse-move-aside {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  .emit-place {
+    position: absolute;
+    left: 0;
+    width: 10px;
+    height: 100%;
+    background: #00ff80;
+  }
+
+  .windows-container {
+    width: 300px;
+    height: 100%;
+    position: fixed;
+    right: 0;
+    top: 0;
+    background: #1c6ca1;
+    opacity: .4;
+  }
+}
+</style>
